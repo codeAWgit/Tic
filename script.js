7a5f1c79bc9449b4db292d3555e032ca6f339d90
codeAWgit <a_hobson@hotmail.com>
2018-03-05 11:41:31 -0600

    Completed first user experience: Picking X or O player.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 'use strict'
 
-$('#menu').slideDown(2000)
+$('#menu').slideDown(1800)
 
+$('#X').click( function(){
+    $('#menu').slideUp(1000)
+    setTimeout(youGo, 1000)
+
+    function youGo(){
+        $('#youGo').show()
+        setTimeout(() => $('#youGo').hide(), 2000)
+    }
+
+    playBall()
+})
+
+$('#O').click( function(){
+    $('#menu').slideUp(1000)
+
+    playBall()
+})
+
+function playBall() {
+    
+}
