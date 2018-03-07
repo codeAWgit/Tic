de4e616bea700cd1b577d4e1d8f2258f22c4b31e
codeAWgit <a_hobson@hotmail.com>
2018-03-07 05:47:04 -0600

    JS in place to play a game and see a banner when game ends.

--- script.js
+++ script.js
@@ -1,25 +1,83 @@
 'use strict'
+// sqs is short for squares. Top left is [0].
+let disableClick
 
-$('#menu').slideDown(1800)
+function gamePlay () {
 
-$('#X').click( function(){
-    $('#menu').slideUp(1000)
-    setTimeout(youGo, 1000)
+    let sqs = document.querySelectorAll('.col-3')
+    let comp, player, boardSpace = []
 
-    function youGo(){
-        $('#youGo').show()
-        setTimeout(() => $('#youGo').hide(), 2000)
+    $('#menu').slideDown(1800)
+
+    $('#X').click( function(){
+        $('#menu').slideUp(1000)
+        comp = 'O'; player = 'X';
+        setTimeout(youGo, 1000)
+
+        function youGo(){
+            $('#youGo').show()
+            setTimeout(() => $('#youGo').hide(), 2000)
+            setTimeout(() => enableClick(), 2000)
+        }
+    })
+
+    $('#O').click( function(){
+        $('#menu').slideUp(1000)
+        comp = 'X'; player = 'O';
+        playBall()
+    })
+
+    function enableClick() {
+        let len = sqs.length 
+
+        for ( let i = 0; i < len; i++) {
+            if (!boardSpace.includes(i)) {
+                sqs[i].attributes[1].value = `this.innerHTML = "${player}"; disableClick(${i})`
+            }
+        }
     }
 
-    playBall()
-})
+    disableClick = function (sqreClicked) {
+        let len = sqs.length
+        boardSpace.push(+sqreClicked)
 
-$('#O').click( function(){
-    $('#menu').slideUp(1000)
+        for( let i = 0; i < len; i++) {
+            sqs[i].attributes[1].value = ''
+        }
 
-    playBall()
-})
+        if (boardSpace.length > 4) checkForWinner()
+        playBall()
+    }
 
-function playBall() {
-    
+    function playBall() {
+        let x = Math.floor(Math.random() * 9)
+        if (!boardSpace.includes(x)) {
+            boardSpace.push(x)
+            sqs[x].innerHTML = comp
+
+            if (boardSpace.length > 4) checkForWinner()
+            enableClick()
+        }
+        else {
+            playBall()
+        }
+    }
+
+    function checkForWinner () {
+        if ( boardSpace.length === 9) {
+            $('#youGo').text('Draw game. Try again.')
+            $('#youGo').show()
+            boardSpace = []
+            setTimeout(() => $('#youGo').hide(), 2000)
+            
+            let len = sqs.length
+            for ( let i = 0; i < len; i++) {
+                    sqs[i].innerHTML = ''
+            }
+
+            gamePlay()
+        }
+    }
 }
+
+gamePlay()
