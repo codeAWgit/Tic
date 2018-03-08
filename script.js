73468ec367253e4918fc6bbeb570d81ff13c920a
codeAWgit <a_hobson@hotmail.com>
2018-03-08 12:34:28 -0600

    Game resets now to play again after full board. Now I need to establish the rules for winning a tictactoe game.

--- script.js
+++ script.js
@@ -1,83 +1,99 @@
 'use strict'
 // sqs is short for squares. Top left is [0].
 let disableClick
+let sqs = document.querySelectorAll('.col-3')
+let comp, player, boardSpace
 
-function gamePlay () {
+startNewGame()
 
-    let sqs = document.querySelectorAll('.col-3')
-    let comp, player, boardSpace = []
-
+function startNewGame () {
     $('#menu').slideDown(1800)
 
     $('#X').click( function(){
+        
         $('#menu').slideUp(1000)
         comp = 'O'; player = 'X';
         setTimeout(youGo, 1000)
 
         function youGo(){
+            $('#youGo').text('X goes first.')
             $('#youGo').show()
             setTimeout(() => $('#youGo').hide(), 2000)
-            setTimeout(() => enableClick(), 2000)
-        }
-    })
 
-    $('#O').click( function(){
-        $('#menu').slideUp(1000)
-        comp = 'X'; player = 'O';
-        playBall()
-    })
-
-    function enableClick() {
-        let len = sqs.length 
-
-        for ( let i = 0; i < len; i++) {
-            if (!boardSpace.includes(i)) {
-                sqs[i].attributes[1].value = `this.innerHTML = "${player}"; disableClick(${i})`
-            }
-        }
-    }
-
-    disableClick = function (sqreClicked) {
-        let len = sqs.length
-        boardSpace.push(+sqreClicked)
-
-        for( let i = 0; i < len; i++) {
-            sqs[i].attributes[1].value = ''
-        }
-
-        if (boardSpace.length > 4) checkForWinner()
-        playBall()
-    }
-
-    function playBall() {
-        let x = Math.floor(Math.random() * 9)
-        if (!boardSpace.includes(x)) {
-            boardSpace.push(x)
-            sqs[x].innerHTML = comp
-
-            if (boardSpace.length > 4) checkForWinner()
-            enableClick()
-        }
-        else {
-            playBall()
-        }
-    }
-
-    function checkForWinner () {
-        if ( boardSpace.length === 9) {
-            $('#youGo').text('Draw game. Try again.')
-            $('#youGo').show()
             boardSpace = []
-            setTimeout(() => $('#youGo').hide(), 2000)
-            
+
             let len = sqs.length
             for ( let i = 0; i < len; i++) {
                     sqs[i].innerHTML = ''
             }
 
-            gamePlay()
+            setTimeout(() => enableClick(), 2000)
+        }
+    })
+
+    $('#O').click( function(){
+
+        $('#menu').slideUp(1000)
+        comp = 'X'; player = 'O';
+        
+        boardSpace = []
+
+        let len = sqs.length
+        for ( let i = 0; i < len; i++) {
+                sqs[i].innerHTML = ''
+        }
+
+        playBall()
+    })
+}
+
+function enableClick() {
+    let len = sqs.length 
+
+    for ( let i = 0; i < len; i++) {
+        if (!boardSpace.includes(i)) {
+            sqs[i].attributes[1].value = `this.innerHTML = "${player}"; disableClick(${i})`
         }
     }
 }
 
-gamePlay()
+disableClick = function (sqreClicked) {
+    let len = sqs.length
+    boardSpace.push(+sqreClicked)
+
+    for( let i = 0; i < len; i++) {
+        sqs[i].attributes[1].value = ''
+    }
+
+    if (boardSpace.length > 4) checkForWinner()
+    playBall()
+}
+
+function playBall() {
+    let x = Math.floor(Math.random() * 9)
+    if (!boardSpace.includes(x)) {
+        boardSpace.push(x)
+        sqs[x].innerHTML = comp
+
+        if (boardSpace.length > 4) checkForWinner()
+        enableClick()
+    }
+    else {
+        playBall()
+    }
+}
+
+function checkForWinner () {
+    if ( boardSpace.length === 9) {
+        $('#youGo').text('Draw game. Try again.')   
+        $('#youGo').show()
+        setTimeout(() => $('#youGo').hide(), 2000)
+        player = ''
+        startNewGame()
+    }
+    else {
+
+    }
+}
+
+
